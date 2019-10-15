6ade9e119fae13c8ac4a25ab9cb15d1c8ffc1c20
NoNameSteve <cooperRoboticsBooster@gmail.com>
2019-10-14 20:30:16 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+    // Motors
+    public static final int LEFT_FRONT = 0;
+    public static final int LEFT_BACK = 3;
+    public static final int RIGHT_FRONT = 1;
+    public static final int RIGHT_BACK = 2;
+    // Controllers
+    public static final int XBOX_USB = 0;
+}
